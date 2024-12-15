89d34831c58056f1bb32b5dbf28cf19c623f32ce
edwinvg99 <velasquezgiraldoedwin@gmail.com>
2024-12-14 23:18:39 -0500

    Configuración para Railway

---
+++ Api_Reflex/Procfile.py
@@ -0,0 +1,1 @@
+web: reflex run --no-frontend
--- rxconfig.py
+++ rxconfig.py
@@ -1,8 +1,11 @@
 import reflex as rx
+import os
 
 
 config = rx.Config(
     app_name="Api_Reflex",
     tailwind={},
+    port=int(os.getenv("PORT", 3000)),  # Puerto asignado por Railway
+    host="0.0.0.0",  # Permitir conexiones externas
 
 )
